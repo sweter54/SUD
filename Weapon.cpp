d41d2334a74242b97a0da96832f8055af3c6be64
sweter54 <sweter54@gmail.com>
2015-06-07 23:59:46 +0200

    r

--- Weapon.cpp
+++
@@ -1,9 +0,0 @@
-#include "Weapon.hpp"
-#include <iostream>
-
-using namespace std;
-
-Weapon::Weapon(int crit,int damage,std::string a,std::string b) : Item(a,b), crit(crit), damage(damage)
-{
-    
-}
