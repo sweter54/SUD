4fa465eb8a7bc6e7a80126cb5585f6de71b89fdf
sweter54 <sweter54@gmail.com>
2015-06-09 20:15:55 +0200

    Update champs.cpp

--- champs.cpp
+++ champs.cpp
@@ -1,70 +1,157 @@
-#ifndef CHAMP_HPP
-#define CHAMP_HPP
-
-class Inventory;
-
-
+#include <cstdio>
+#include "Champ.hpp"
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include "Inventory.hpp"
-#include "mob.hpp"
 
-
-
-class Champ
+Champ::Champ(std::string name1 , std::string tab)
 {
-	friend class Fight;
-	friend class Items;
-	friend  std::ostream& operator<<(std::ostream& out,const Champ& s);
+
+	CarriedArmour[Armour::Slot::torso]=nullptr;
+	CarriedHelmet[Armour::Slot::head]=nullptr;
+	CarriedShoes[Armour::Slot::legs]=nullptr;
+	CarriedWeapon=nullptr;
 
 
-	private:
+	name = name1;
 
-		std::string name;
-		short stun=0;
-		int attack=0;
-		int str=0;
-		int stm=0;
-		int dex=0;
-		int wisd=0;
-		int deff=0;
-		int HP=0;
-		int level=0;
-		int exp=0;
-		int maxExp=100;
-		int itmId;
+	if(tab.size()==7)WarriorCr();
+	if(tab.size()==4)MageCr();
+	if(tab.size()==5)RogueCr();
 
-		Armour* CarriedArmour[Armour::Slot::torso];
-		Armour* CarriedHelmet[Armour::Slot::head];
-		Armour* CarriedShoes[Armour::Slot::legs];
-		Weapon* CarriedWeapon;
+	returnHP();
+	returnDeff();
+	rtnATC();
+}
 
-		std::string guild;
+Champ::Champ(Champ & basic) :
 
-	public:
+	str(basic.str), stm (basic.stm),
+	level(basic.level), dex(basic.dex),
+	wisd(basic.wisd), HP(basic.HP),
+	 deff(basic.deff)
+{
 
-		Inventory inventory;
+}
+
+Champ::~Champ()
+{
+
+}
+
+void Champ::MageCr()
+{
+	srand(time(NULL));
+
+	level=1;
+
+	wisd = 3+rand()%2+1;
+	dex = rand()%3+1;
+	str = rand()%3+1;
+	stm = rand()%3+1;
+
+	printf("Stworzono magusa\n");
+}
+
+void Champ::RogueCr()
+{
+	srand(time(NULL));
+
+	level=1;
+
+	dex = 2+rand()%2+1;
+	str = rand()%3+1;
+	wisd = rand()%3+1;
+	stm = 1+rand()%3+1;
+
+	printf("Stworzono Rogala\n");
+}
+void Champ::WarriorCr()
+{
+	srand(time(NULL));
+
+	level=1;
+
+	str = 2+rand()%2+1;
+	stm = 1+rand()%3+1;
+	dex = rand()%3+1;
+	wisd = rand()%3+1;
+
+	printf("Stworzono warjora\n");
+}
+
+std::ostream& operator<<(std::ostream& out,const Champ& s)
+{
+		out<<"Wybrana Postać: "<<s.name<<"\n"<<"Posiada: \n"<<s.exp<<"doswiadczenia\n"<<
+		s.str<<" Sily\n"<<s.stm<<" wytrzymalosci\n"<<s.dex<<" Zrecznosci\n"<<s.wisd<<" Madrosci \n"<<s.deff<<" obrony \n"<<s.HP<<" Zycia\n";
+
+		return out;
+}
+void Champ::equipWeapon(Weapon* weapon)
+{
+	this->CarriedWeapon=weapon;
+
+}
+
+void Champ::equipArmour(Armour* armour)
+{
+	if(((int)armour->slot==1))
+	{
+		this->CarriedArmour[(int)armour->slot]=armour;
+	}
+	else if(((int)armour->slot==2))
+	{
+		this->CarriedHelmet[(int)armour->slot]=armour;
+	}
+	if(((int)armour->slot==3))
+	{
+		this->CarriedShoes[(int)armour->slot]=armour;
+	}
+	else
+	{
+		cerr<<"Blad Przy zakladaniu pancerza";
+		return;
+	}
+}
 
 
-		Champ(std::string,std::string);
-		Champ(Champ&);
-		~Champ();
+void Champ::returnHP()
+{
+	HP= 30*stm;
+}
 
+void Champ::returnDeff()
+{
+	deff= 3*dex;
+}
 
-		int rtnLVL();
-		int ShowHP();
-		int mExp();
+int Champ::rtnLVL()
+{
+	return level;
+}
 
-		void rtnATC();
-		void returnDeff();
-		void returnHP();
-		void KnightCr();
-		void MageCr();
-		void RogueCr();
-		void WarriorCr();
-		void HPregen();
+int Champ::ShowHP()
+{
+	return HP;
+}
 
-		void equipWeapon(Weapon* weapon);
-		void equipArmour(Armour* armour);
-};
-#endif
+int Champ::mExp()
+{
+	if(maxExp==exp)
+	{
+		level++;
+		maxExp=exp*exp*64;
+	}
+	else return maxExp;
+
+}
+void Champ::rtnATC()
+{
+	attack=str*6;
+}
+
+void Champ::HPregen()
+{
+	HP=30*stm;
+}
